1ef8908d9852bb8d654ea8aa435712f96cb2868c
isaccchoi <isaccchoi@naver.com>
2017-11-07 16:22:28 +0900

    cbv viewset router 사용

--- django_rest_framewokr_tutorial/snippets/urls/__init__.py
+++ django_rest_framewokr_tutorial/snippets/urls/__init__.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 
-from . import cbv, fbv, cbv_mixins, cbv_generics, cbv_viewsets
+from . import cbv, fbv, cbv_mixins, cbv_generics, cbv_viewsets_explicitly, cbv_viewsets_router
 
 urlpatterns = [
     url(r'^fbv/', include(fbv, namespace='fbv')),
     url(r'^cbv/', include(cbv, namespace='cbv')),
     url(r'^cbv_mixins/', include(cbv_mixins, namespace='cbv_mixins')),
     url(r'^cbv_generics/', include(cbv_generics, namespace='cbv_generics')),
-    url(r'^cbv_viewssets/', include(cbv_viewsets, namespace='cbv_viewsets')),
+    url(r'^cbv_viewsets/', include(cbv_viewsets_explicitly, namespace='cbv_viewsets_excplicitly')),
+    url(r'^cbv_viewsets_router/', include(cbv_viewsets_router, namespace='cbv_viewsets_router')),
 ]
--- django_rest_framewokr_tutorial/snippets/urls/cbv_viewsets_explicitly.py
+++ django_rest_framewokr_tutorial/snippets/urls/cbv_viewsets_explicitly.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
 
 from ..views.cbv_viewsets import SnippetViewSet
 
@@ -18,5 +17,3 @@ urlpatterns = [
     url(r'^$', snippet_list, name='snippet_list'),
     url(r'^(?P<pk>\d+)/$', snippet_detail, name='snippet_detail'),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
